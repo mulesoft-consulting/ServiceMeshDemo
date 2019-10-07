3a0342883461dd9da3bbd2328877cd55a7bc54ca
Anthony Chaseendran <achaseendran@achaseend-ltm.internal.salesforce.com>
2019-10-07 13:46:38 -0700

    updated UI build process

--- service-mesh-ui/gulpfile.js
+++ service-mesh-ui/gulpfile.js
@@ -1,26 +1,78 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var header = require('gulp-header');
 var browserSync = require('browser-sync').create();
+var clean = require('gulp-clean');
+var argv = require('minimist')(process.argv.slice(2));
+//parameterize initialization
+//run delete of dist before bundling
 
-gulp.task('browserSync', function() {
-    browserSync.init({
-        injectChanges: true,
-        notify: false,
-        server: {
-            baseDir: './'
-        }
-    })
-})
+console.log("CUSTOMER_URL is: ", argv.c);
+console.log("PAYMENT_URL is: ", argv.p);
+console.log("ORDER_URL is: ", argv.o);
+
+// gulp  bundle -c "http://52.168.30.92:8080" -o "http://52.224.219.48:8080" -p "http://40.114.70.224:8080"
 
 gulp.task('sass', function() {
     return gulp.src('scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('css'))
-        .pipe(browserSync.stream({match: '**/*.css'}));
+        .pipe(browserSync.stream({ match: '**/*.css' }));
 })
 
-gulp.task('watch', ['browserSync', 'sass'], function() {
-    gulp.watch('scss/**/*.scss', ['sass']);
-    gulp.watch('**/*.html', browserSync.reload);
-    gulp.watch('js/**/*.js', browserSync.reload);
+gulp.task('reload', function(done) {
+    browserSync.reload();
+    done();
 })
+
+gulp.task('watch', function() {
+    browserSync.init({
+        injectChanges: true,
+        notify: false,
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('scss/**/*.scss', gulp.series('sass'));
+    gulp.watch(['**/*.html', 'js/**/*.js'], gulp.series('reload'));
+})
+
+gulp.task('serve', function() {
+    browserSync.init({
+        injectChanges: true,
+        notify: false,
+        server: {
+            baseDir: "./dist"
+        }
+    });
+})
+
+gulp.task('pack-js', function() {
+    return gulp.src(['./js/**/*.js'])
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('pack-css', function() {
+    return gulp.src(['./css/**/*.css'])
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('pack-html', function() {
+    return gulp.src(['./**/*.html', '!node_modules/**/*', '!dist/**/*'])
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('inject-url', function() {
+    return gulp.src(['./dist/js/main.js'])
+        .pipe(header("var CUSTOMER_URL = '" + argv.c + "';"))
+        .pipe(header("var PAYMENT_URL = '" + argv.p + "';"))
+        .pipe(header("var ORDER_URL = '" + argv.o + "';"))
+        .pipe(gulp.dest('./dist/js/'));
+})
+
+gulp.task('clean', function() {
+    return gulp.src('./dist', { read: false, allowEmpty: true })
+        .pipe(clean());
+});
+
+gulp.task('bundle', gulp.series(['clean', 'sass', 'pack-js', 'inject-url', 'pack-css', 'pack-html', 'serve']))
--- service-mesh-ui/js/main.js
+++ service-mesh-ui/js/main.js
@@ -1,5 +1,7 @@
-const LOCAL_URL = "http://127.0.0.1:9292/api";
-const CLOUD_URL = "http://tshirt-soap-to-rest.cloudhub.io/api";
+// Update and remove comment for Development
+// var CUSTOMER_URL = "http://52.168.30.92:8080";
+// var PAYMENT_URL = "http://40.114.70.224:8080";
+// var ORDER_URL = "http://52.224.219.48:8080";
 var cart = [];
 var app = angular.module('tshirtApp', ['ngRoute']);
 
@@ -9,9 +11,9 @@ var app = angular.module('tshirtApp', ['ngRoute']);
  **/
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
-        .when("/", { templateUrl: "partials/home.html" })
-        .when("/reserve", { templateUrl: "partials/reserve.html", controller: "ProductCtrl" })
-        .when("/account", { templateUrl: "partials/account.html", controller: "AccountCtrl", activePage: "account" })
-        .when("/confirmation", { templateUrl: "partials/confirmation.html", controller: "", activePage: "confirmation" })
-        .otherwise("/", { templateUrl: "partials/home.html" });
+        .when("/", { templateUrl: "../partials/home.html" })
+        .when("/reserve", { templateUrl: "../partials/reserve.html", controller: "ProductCtrl" })
+        .when("/account", { templateUrl: "../partials/account.html", controller: "AccountCtrl", activePage: "account" })
+        .when("/confirmation", { templateUrl: "../partials/confirmation.html", controller: "", activePage: "confirmation" })
+        .otherwise("/", { templateUrl: "../partials/home.html" });
 }]);
